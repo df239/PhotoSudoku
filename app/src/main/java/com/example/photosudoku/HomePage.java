003a705ee72cb0fc300e7b40ea06901e9f8bf827
David Filipiak <df239@canterbury.ac.uk>
2021-10-01 17:59:51 +0100

    added navigation capability between HomePage and CameraPage

--- app/src/main/java/com/example/photosudoku/HomePage.java
+++ app/src/main/java/com/example/photosudoku/HomePage.java
@@ -2,7 +2,9 @@ package com.example.photosudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OpenCamera(View view){
+        Intent cameraPageIntent = new Intent(this,CameraPage.class);
+        startActivity(cameraPageIntent);
     }
 }
