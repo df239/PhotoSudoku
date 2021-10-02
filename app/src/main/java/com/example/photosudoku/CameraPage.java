280218832745b9ae954d6fb23586a79188da57ae
David Filipiak <df239@canterbury.ac.uk>
2021-10-02 19:53:35 +0100

    added camera view capability to the app

--- app/src/main/java/com/example/photosudoku/CameraPage.java
+++ app/src/main/java/com/example/photosudoku/CameraPage.java
@@ -1,14 +1,67 @@
 package com.example.photosudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
 public class CameraPage extends AppCompatActivity {
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    PreviewView previewView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_page);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 100);
+        }
+
+        previewView = (PreviewView)findViewById(R.id.previewView);
+
+        //matches camera process to a single camera process provider
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        //adds a task to the process camera provider
+        cameraProviderFuture.addListener(() -> {
+            try{
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            }catch(ExecutionException e){
+                e.printStackTrace();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }, getExecutor());
+    }
+
+    //return executor that will run enqueued tasks on the main thread associated with the context
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void startCameraX(@NotNull ProcessCameraProvider cameraProvider){
+        //selects the camera to use for the use case
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        //creates a preview object and conencts it to the layout element previewView
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview);
     }
 }
