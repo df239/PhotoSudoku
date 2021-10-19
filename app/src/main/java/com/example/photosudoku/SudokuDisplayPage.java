67a6e61cf7acbf09118a81f83379600672a8d4f8
David Filipiak <df239@canterbury.ac.uk>
2021-10-19 20:32:01 +0100

    sudoku array is now displayed in a table form

--- app/src/main/java/com/example/photosudoku/SudokuDisplayPage.java
+++ app/src/main/java/com/example/photosudoku/SudokuDisplayPage.java
@@ -1,27 +1,42 @@
 package com.example.photosudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
 
 public class SudokuDisplayPage extends AppCompatActivity {
 
     TextView textView;
+    TableLayout table;
+    ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku_display_page);
 
-        textView = (TextView)findViewById(R.id.sudokuTextView);
+        //textView = (TextView)findViewById(R.id.sudokuTextView);
+        table = (TableLayout)findViewById(R.id.sudokuTable);
+        mainLayout = (ConstraintLayout)findViewById(R.id.cameraPageLayout);
 
         Intent intent = getIntent();
         int[][] sudoku = (int[][])intent.getSerializableExtra("sudoku");
-
-        String str = arrayToString(sudoku);
-        textView.setText(str);
+        createSudokuUI(sudoku);
+        //String str = arrayToString(sudoku);
+        //textView.setText(str);
     }
 
     private String arrayToString(int[][] array){
@@ -38,5 +53,27 @@ public class SudokuDisplayPage extends AppCompatActivity {
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    private void createSudokuUI(int[][] sudoku){
+        Drawable thinBg = ContextCompat.getDrawable(this,R.drawable.thin_cell_border);
+        for (int row = 0; row < 9; row++){
+            TableRow tableRow = (TableRow)table.getChildAt(row);
+            for (int col = 0; col < 9; col++){
+
+                EditText cell = (EditText)tableRow.getChildAt(col);
+                cell.setBackground(thinBg);
+//                cell.setEms(1);
+//                cell.setInputType(InputType.TYPE_CLASS_NUMBER);
+//                cell.setLayoutParams(new TableLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT,0.11f));
+//                cell.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+//                cell.setMaxEms(1);
+
+                if (sudoku[row][col] != 0){
+                    cell.setText(String.valueOf(sudoku[row][col]));
+                }
+            }
+        }
+        table.setBackground(ContextCompat.getDrawable(this,R.drawable.thick_cell_border));
     }
 }
