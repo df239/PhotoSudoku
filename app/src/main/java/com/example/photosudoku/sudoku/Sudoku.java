6b986316c7abb504bddf82d3e23486b9bcfdae56
David Filipiak <df239@canterbury.ac.uk>
2021-11-16 21:13:25 +0000

    added testing java project

--- app/src/main/java/com/example/photosudoku/sudoku/Sudoku.java
+++ app/src/main/java/com/example/photosudoku/sudoku/Sudoku.java
@@ -1,6 +1,12 @@
 package com.example.photosudoku.sudoku;
 
 public class Sudoku {
+//    public static final int[][] sample =
+//            {{0,0,8,0,1,0,0,0,9},
+//             {6,0,1,0,9,0,3,2,0},
+//             {}};
+
+
     private int[][] grid;
     private int[][] solution;
     private Cell[][] sudoku;
