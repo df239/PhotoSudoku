8beb78252b0d4f78151a1270628f178c44a1b39e
df239 <df239@canterbury.ac.uk>
2022-02-24 16:12:41 +0000

    added unit tests for initializing sudoku objects

---
+++ app/src/test/java/com/example/photosudoku/SudokuTest.java
@@ -0,0 +1,76 @@
+package com.example.photosudoku;
+
+import android.util.Log;
+
+import com.example.photosudoku.sudoku.Sudoku;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class SudokuTest {
+
+    @Test
+    public void sudoku_init_test(){
+        Sudoku sudoku1 = new Sudoku(TestingSamples.empty);
+        Random random = new Random();
+        for(int i = 0; i < 10; i++){
+            int randRow = random.nextInt(9);
+            int randCol = random.nextInt(9);
+            assertFalse(sudoku1.getCellMatrix()[randRow][randCol].solved());
+        }
+
+        Sudoku sudoku2 = new Sudoku(TestingSamples.defaultFull);
+        assertTrue(sudoku2.solved());
+        for(int i = 0; i < 10; i++){
+            int randRow = random.nextInt(9);
+            int randCol = random.nextInt(9);
+            assertTrue(sudoku2.getCellMatrix()[randRow][randCol].solved());
+        }
+
+        Sudoku sudoku3 = new Sudoku(TestingSamples.sample1);
+        int[] candidatePlaces = new int[]{0,8,6,5,3,1,4,7,4,4};  //tuples of row and col indices
+        for(int i = 0; i < 5; i++){
+            assertFalse(sudoku3.getCellMatrix()[candidatePlaces[2*i]][candidatePlaces[2*i+1]].solved());
+        }
+        int[] solvedPlaces = new int[]{0,0,8,8,6,3,3,2,7,5};   //tupes of row and col indices
+        for(int i = 0; i <solvedPlaces.length/2; i++){
+            assertTrue(sudoku3.getCellMatrix()[solvedPlaces[2*i]][solvedPlaces[2*i+1]].solved());
+        }
+    }
+
+    @Test
+    public void sudoku_candidates_init_test(){
+        Sudoku sudoku1 = new Sudoku(TestingSamples.empty);
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        HashSet<Integer> allCandidates = new HashSet<>(list);
+        Random random = new Random();
+        for(int i = 0; i < 10; i++){
+            int randRow = random.nextInt(9);
+            int randCol = random.nextInt(9);
+            HashSet<Integer> actual = new HashSet<>(sudoku1.getCellMatrix()[randRow][randCol].getCandidates());
+            assertEquals(allCandidates,actual);
+        }
+
+        Sudoku sudoku2 = new Sudoku(TestingSamples.defaultFull);
+        for(int i = 0; i < 10; i++){
+            int randRow = random.nextInt(9);
+            int randCol = random.nextInt(9);
+            HashSet<Integer> actual = new HashSet<>(sudoku2.getCellMatrix()[randRow][randCol].getCandidates());
+            assertEquals(new HashSet<Integer>(), actual);
+        }
+
+        Sudoku sudoku3 = new Sudoku(TestingSamples.sample1);
+        int[] candidateAmounts = new int[]{1,2,3,4,5};
+        int[] candidatePlaces = new int[]{0,8,6,5,3,1,4,7,4,4};  //tuples of row and col indices
+        for(int i = 0; i < candidateAmounts.length; i++){
+            int len = sudoku3.getCellMatrix()[candidatePlaces[2*i]][candidatePlaces[2*i+1]].getCandidates().size();
+            assertEquals(candidateAmounts[i],len);
+        }
+    }
+}
